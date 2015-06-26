12ee50d40b75de320ed5a4ac4d5ec24355d252a4
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-06-25 21:20:44 -0500

    June 25 commit — under construction

--- js/main.js
+++ js/main.js
@@ -6,6 +6,39 @@ $(document).ready(function() {
         selector: 'textarea.rich-text',
         statusbar: false,
         resize: true,
-        height: 198
+        height: 190
+    });
+});
+
+/* ----------------------------------------
+jQuery UI Autocomplete Initialize
+---------------------------------------- */
+$(document).ready(function() {
+    var choices = [
+        'ActionScript',
+        'AppleScript',
+        'Asp',
+        'BASIC',
+        'C',
+        'C++',
+        'Clojure',
+        'COBOL',
+        'ColdFusion',
+        'Erlang',
+        'Fortran',
+        'Groovy',
+        'Haskell',
+        'Java',
+        'JavaScript',
+        'Lisp',
+        'Perl',
+        'PHP',
+        'Python',
+        'Ruby',
+        'Scala',
+        'Scheme'
+    ];
+    $('.autocomplete').autocomplete({
+        source: choices
     });
 });
