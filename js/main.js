f9519d320fd2f14341a815a4c0f95c65777cf855
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-07-30 10:56:56 -0500

    Updated Quick Modal to 1.03

--- js/main.js
+++ js/main.js
@@ -138,121 +138,135 @@ $(document).ready(function() {
 /* ----------------------------------------
 Quick Modal v1.03 Plugin (by Kevin Beronilla; edited for Salesforce1)
 ---------------------------------------- */
-$.fn.setSpeed = function(speed) {
-    this.css('-webkit-transition-duration', speed + 'ms')
-        .css('-moz-transition-duration', speed + 'ms')
-        .css('-ms-transition-duration', speed + 'ms')
-        .css('-o-transition-duration', speed + 'ms')
-        .css('transition-duration', speed + 'ms');
-}
-
-$.fn.setTiming = function(timing) {
-    this.css('-webkit-transition-timing-function', timing)
-        .css('-moz-transition-timing-function', timing)
-        .css('-ms-transition-timing-function', timing)
-        .css('-o-transition-timing-function', timing)
-        .css('transition-timing-function', timing);
-}
-
-function checkSettings(modalObj, backgroundObj, settings) {
-    modalObj.setSpeed(null);
-    modalObj.setTiming(null);
-    backgroundObj.setSpeed(null);
-    backgroundObj.setTiming(null);
-
-    if (settings.speed != 250 || settings.timing != 'ease') { // Set CSS if settings not equal to default
-        modalObj.setSpeed(settings.speed);
-        modalObj.setTiming(settings.timing);
-        backgroundObj.setSpeed(settings.speed);
-        backgroundObj.setTiming(settings.timing);
+(function($) { // Protect the $ alias (IIF)
+    $.fn.setSpeed = function(speed) {
+        this.css('-webkit-transition-duration', speed + 'ms')
+            .css('-moz-transition-duration', speed + 'ms')
+            .css('-ms-transition-duration', speed + 'ms')
+            .css('-o-transition-duration', speed + 'ms')
+            .css('transition-duration', speed + 'ms');
     }
-}
-
-$.fn.quickModal = function(args, options) {
-    if (!$('#modal-background').length) $('body .scope').append('<div id="modal-background"></div>'); // Append background; do not append if re-initialized or background already exists
-
-    if (args !== null && typeof args === 'string') { // If calling a method
-        var settings = $.extend({ // Extend the default settings established below
-                animation: 'fade-up',
-                speed: 250,
-                timing: 'ease',
-                closeModalSelector: '.close-modal'
-            }, options),
-            bodyTag = $('body'),
-            closeModalLink = $(settings.closeModalSelector),
-            modalBackground = $('#modal-background'),
-            targetModal = this,
-            modal = $('.modal');
+    
+    $.fn.setTiming = function(timing) {
+        this.css('-webkit-transition-timing-function', timing)
+            .css('-moz-transition-timing-function', timing)
+            .css('-ms-transition-timing-function', timing)
+            .css('-o-transition-timing-function', timing)
+            .css('transition-timing-function', timing);
+    }
+    
+    function checkSettings(modalObj, backgroundObj, settings) {
+        modalObj.setSpeed(null);
+        modalObj.setTiming(null);
+        backgroundObj.setSpeed(null);
+        backgroundObj.setTiming(null);
         
-        checkSettings(targetModal, modalBackground, settings);
-        
-        targetModal.removeClass()
-            .addClass('modal')
-            .addClass('animation-' + settings.animation);
-        
-        function keyUpCheck(e) {
-            if (e.keyCode == 27 && modal.is(':visible')) { // Esc
-                targetModal.quickModal('close', settings);
-            }
+        if (settings.speed != 250 || settings.timing != 'ease') { // Set CSS if settings not equal to default
+            modalObj.setSpeed(settings.speed);
+            modalObj.setTiming(settings.timing);
+            backgroundObj.setSpeed(settings.speed);
+            backgroundObj.setTiming(settings.timing);
         }
+    }
+    
+    $.fn.quickModal = function(args, options) {
+        if (!$('#modal-background').length) $('body .scope').append('<div id="modal-background"></div>'); // Append background; do not append if background already exists
         
-        switch (args) {
-            case 'open':
-                bodyTag.addClass('disable-scroll');
-                modalBackground.show();
-                targetModal.show();
-                setTimeout(function() { // Ensure elements are displayed before adding classes
-                    modalBackground.addClass('visible');
-                    targetModal.addClass('visible');
-                }, 25);
-                targetModal.trigger('modalopen'); // Trigger custom 'open' event
-                
-                closeModalLink.unbind('click') // Unbind previously bound events to remove lingering settings
-                    .click(function(e) { // Bind events based on options
-                        e.preventDefault();
-                        targetModal.quickModal('close', settings);
-                    });
-                
-                $(document).keyup(keyUpCheck);
-                
-                modalBackground.unbind('click')
-                    .click(function() { // Unbind previously bound events to remove lingering settings
-                        targetModal.quickModal('close', settings);
-                    });
-                break;
+        if (args !== null && typeof args === 'string') { // If calling a method
+            var settings = $.extend({ // Extend the default settings established below
+                    animation: 'fade-up',
+                    speed: 250,
+                    timing: 'ease',
+                    closeModalSelector: '.close-modal',
+                    enableEsc: true,
+                    enableClickAway: true
+                }, options),
+                bodyTag = $('body'),
+                closeModalLink = $(settings.closeModalSelector),
+                modalBackground = $('#modal-background'),
+                selector = this,
+                modal = $('.modal');
             
-            case 'close':
-                if (targetModal.is(':visible')) {
+            checkSettings(selector, modalBackground, settings);
+            
+            modal.removeClass()
+                .addClass('modal')
+                .addClass('animation-' + settings.animation);
+            
+            switch (args) {
+                case 'open':
+                    function keyUpCheck(e) {
+                        if (e.keyCode == 27 && modal.is(':visible') && settings.enableEsc) { // Esc
+                            selector.quickModal('close', settings);
+                        }
+                    }
+                    
+                    modal.hide(); // Hide any currently visible modals
+                    $(document).unbind('keyup', keyUpCheck); // Unbind lingering events
+                    bodyTag.addClass('disable-scroll');
+                    modalBackground.show();
+                    selector.show();
+                    setTimeout(function() { // Ensure elements are displayed before adding classes
+                        modalBackground.addClass('visible');
+                        selector.addClass('visible');
+                    }, 25);
+                    selector.trigger('modalopen'); // Trigger custom 'open' event
+                    
+                    closeModalLink.unbind('click') // Unbind previously bound events to remove lingering settings
+                        .click(function(e) { // Bind events based on options
+                            e.preventDefault();
+                            selector.quickModal('close', settings);
+                        });
+                    
+                    $(document).keyup(keyUpCheck);
+                    
+                    modalBackground.unbind('click'); // Unbind previously bound events to remove lingering settings
+                    
+                    if (settings.enableClickAway) {
+                        modalBackground.click(function() {
+                            selector.quickModal('close', settings);
+                        });
+                    }
+                    break;
+                    
+                case 'close':
                     bodyTag.removeClass('disable-scroll');
                     modalBackground.removeClass('visible');
-                    targetModal.removeClass('visible');
+                    selector.removeClass('visible');
                     setTimeout(function() {
                         modalBackground.hide();
-                        targetModal.hide();
-                        targetModal.trigger('modalclose'); // Trigger custom 'close' event
+                        selector.hide();
+                        selector.trigger('modalclose'); // Trigger custom 'close' event
                     }, settings.speed);
-                } else {
-                    console.error('Target modal is not currently visible.');
-                }
-                break;
+                    break;
+                    
+                case 'trigger':
+                    var modalId = selector.data('modal-id'),
+                    targetModal = $('#' + modalId);
+                    
+                    targetModal.quickModal('open', settings);
+                    break;
+                    
+                default:
+                    console.error('The method you entered does not exist.');
+            }
+        } else { // If initializing plugin with options
+            var openModalLink = this;
+            
+            openModalLink.click(function(e) {
+                e.preventDefault();
+                
+                var modalId = $(this).data('modal-id'),
+                    targetModal = $('#' + modalId);
+                
+                if (modalId === undefined) console.error('No "data-modal-id" attribute is set.');
+                else targetModal.quickModal('open', args);
+            });
         }
-    } else { // If initializing plugin with options
-        var openModalLink = this;
         
-        openModalLink.click(function(e) {
-            e.preventDefault();
-            
-            var modalId = $(this).data('modal-id'),
-                targetModal = $('#' + modalId);
-            
-            if (modalId === undefined) console.error('No "data-modal-id" attribute is set.');
-            
-            targetModal.quickModal('open', args);
-        });
+        return this; // Return the object to enable chaining
     }
-
-    return this; // Return the object to enable chaining
-}
+}(jQuery));
 
 $(document).ready(function() {
     $('.js-open-modal').quickModal(); // Initialize plugin
